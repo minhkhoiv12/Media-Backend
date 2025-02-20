331aece8f86b3a40f82104c17bdfb105ff300018
websitecuakhoi <khoitk14033@gmail.com>
2025-02-20 10:57:33 +0700

    setting-up-apis

---
+++ src/main/java/com/social/media/models/Post.java
@@ -0,0 +1,23 @@
+package com.social.media.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @ManyToOne
+    @JoinColumn(name ="user_id")
+    @JsonIgnore
+    private SocialUser socialUser;
+
+
+}
